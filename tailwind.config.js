d4b632afe2b1cc3936e8d302f79d290d3a5fdf62
mukky254 <muhidinyusuf06@gmail.com>
2025-11-07 01:49:50 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,11 @@ module.exports = {
           800: '#166534',
           900: '#14532d',
         }
-      }
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
     },
   },
   plugins: [],
