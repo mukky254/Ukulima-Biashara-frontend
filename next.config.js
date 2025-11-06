140485ab3254115600bcdc2f8f8cae6b26c946a1
mukky254 <muhidinyusuf06@gmail.com>
2025-11-07 00:02:29 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['res.cloudinary.com', 'localhost'],
+  },
+  env: {
+    API_URL: process.env.API_URL,
+  }
+}
+
+module.exports = nextConfig
